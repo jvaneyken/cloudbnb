e2d63b9e7e1994f51f457e8e01babc23d7610b94
Josh van Eyken <josh.vaneyken@gmail.com>
2023-04-22 14:14:00 -0700

    finish user auth phase 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # post 'api/test', to: 'application#test'
+
+  namespace :api, defaults: { format: :json} do
+    resources :users, only: [:create]
+    resource :session, only: [:show, :create, :destroy]
+    # resources :listings, only: [:index]
+    # resources :reservations
+    # resources :locations, only: [:index]
+    # resources :reviews
+  end
+
 end
