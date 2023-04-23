26d60444601b9b991cac790cba6b57a189da56bc
Josh van Eyken <josh.vaneyken@gmail.com>
2023-04-23 16:45:40 -0700

    restore session user

--- frontend/src/store/csrf.js
+++ frontend/src/store/csrf.js
@@ -14,16 +14,7 @@ async function csrfFetch(url, options = {}){
     return res;
 }
 
-export function storeCSRFToken(response) {
-    const csrfToken = response.headers.get("X-CSRF-Token");
-    if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
-}
 
-export async function restoreCSRF() {
-    const res = await csrfFetch('/api/session');
-    storeCSRFToken(res);
-    return res;
-}
 
 
 
--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -12,6 +12,11 @@ export const removeCurrentUser = () => ({
     type: REMOVE_CURRENT_USER
 })
 
+const storeCSRFToken = (response) => {
+  const csrfToken = response.headers.get("X-CSRF-Token");
+  if (csrfToken) sessionStorage.setItem("X-CSRF-Token", csrfToken);
+}
+
 const storeCurrentUser = user => {
     if (user) sessionStorage.setItem("currentUser", JSON.stringify(user));
     else sessionStorage.removeItem("currentUser");
@@ -27,16 +32,28 @@ const storeCurrentUser = user => {
       })
     });
     const data = await response.json();
+    storeCurrentUser(data.user);
     dispatch(setCurrentUser(data.user));
     return response;
   };
 
-  const initialState = { user: null };
+  export const restoreSession = () => async dispatch => {
+    const response = await csrfFetch("/api/session");
+    storeCSRFToken(response);
+    const data = await response.json();
+    storeCurrentUser(data.user);
+    dispatch(setCurrentUser(data.user));
+    return response;
+  };
+
+  const initialState = { 
+    user: JSON.parse(sessionStorage.getItem("currentUser"))
+  };
   
   const sessionReducer = (state = initialState, action) => {
     switch (action.type) {
       case SET_CURRENT_USER:
-        return { ...state, user: action.payload };
+        return { ...state, user: action.user };
       case REMOVE_CURRENT_USER:
         return { ...state, user: null };
       default:
