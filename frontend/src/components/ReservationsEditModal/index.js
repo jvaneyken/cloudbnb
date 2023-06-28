93f4f723dbf2bee52f0c2e6301bbfc28a43ac372
Josh van Eyken <josh.vaneyken@gmail.com>
2023-06-27 17:03:01 -0700

    Change reservation form date input so that you cannot select a date in the past

--- frontend/src/components/ReservationsEditModal/index.js
+++ frontend/src/components/ReservationsEditModal/index.js
@@ -19,6 +19,13 @@ const ReservationsEditModal = ({currentReservation, closeModal}) => {
 
     const guestOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+    const getCurrentDate = () => {
+        let currentDate = '';
+        const d = new Date();
+        currentDate += d.getFullYear() + "-" + (String(d.getMonth() + 1).padStart(2, '0')) + "-" + String(d.getDate()).padStart(2, '0');
+        return currentDate;
+    }   
+
     return(
         <div id="reservation-update-modal-background">
             <div id='reservation-update-modal'>
@@ -26,8 +33,8 @@ const ReservationsEditModal = ({currentReservation, closeModal}) => {
                     <button className='reservation-update-cancel-button' onClick={() => closeModal()} >X</button>
                 <div id='reservation-update-title'>Update your Reservation!</div>
                     <form id='reservation-update-form' >
-                        <input className='reservation-update-date-input' type="date" onChange={((e)=> setCheckInDate(e.target.value))} value={checkInDate} />
-                        <input className='reservation-update-date-input' type="date" onChange={((e)=> setCheckOutDate(e.target.value))} value={checkOutDate} />
+                        <input className='reservation-update-date-input' type="date" onChange={((e)=> setCheckInDate(e.target.value))} value={checkInDate} min={getCurrentDate()} />
+                        <input className='reservation-update-date-input' type="date" onChange={((e)=> setCheckOutDate(e.target.value))} value={checkOutDate} min={checkInDate} />
                         <select onChange={((e)=> setNumGuests(e.target.value))} value={numGuests} id='reservation-update-select-num-guests'>
                                 <option disabled>Select Number of guests</option>
                                 {guestOptions.map((option) => (
